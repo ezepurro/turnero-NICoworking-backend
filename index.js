6d4a30a79393c33d97f8decb794fe6e23a8f7dbc
Ezequiel Purro <ezequiel.purro@gmail.com>
2025-03-25 22:52:28 -0300

    important changes in cors

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import fs from 'fs'; 
-import https from 'https';
 import cookieParser from 'cookie-parser';
 import config from "./config.js";
 import appointmentRouter from './routes/appointments.js';
@@ -14,10 +12,7 @@ import whatsappRouter from './routes/whatsapp.js';
 const app = express();
 
 // Cors
-app.use(cors({
-    origin: config.FRONTEND_BASE_URL,
-    credentials: true
-}));
+app.use(cors());
 
 // Middleware para parsear cookies
 app.use(cookieParser());
@@ -32,20 +27,8 @@ app.use('/api/settings', settingsRouter);
 app.use('/api/mercadopago', mpRouter);
 app.use('/api/whatsapp', whatsappRouter);
 
-// Leer los archivos del certificado y la clave privada
-// const options = {
-//     key: fs.readFileSync('localhost-key.pem'),
-//     cert: fs.readFileSync('localhost.pem')
-// };
-
 // Escuchar peticiones
 app.listen(process.env.PORT, async () => {
     console.log(`Servidor corriendo en puerto ${config.PORT}`);
     await import("./cronJob/cronJob.js");
 });
-
-// // Crear el servidor HTTPS
-// https.createServer(options, app).listen(config.PORT, async () => {
-//     console.log(`Servidor HTTPS corriendo en puerto ${config.PORT}`);
-//     await import("./cronJob/cronJob.js");
-// });
